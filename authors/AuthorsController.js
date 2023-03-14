962093ca11b44a632144bddc95845c8474722746
CandiceBrasileiro <candicelopes88@gmail.com>
2023-03-14 09:27:12 -0300

    update

--- authors/AuthorsController.js
+++ authors/AuthorsController.js
@@ -62,7 +62,7 @@ router.post("/authors/", (req, res) => {
     })
   })
 })
-
+// tá "deletando" id que não existem
 router.delete("/authors/:id", (req, res) => {
   let id_d = req.params.id;
 console.log(id_d)
@@ -84,5 +84,46 @@ console.log(id_d)
           .send({ message: `Não foi possível excluir o autor ${id_d}` });
       });
     });
+
+    // router.get("/authors/edit/:id", (req, res) => {
+    //   let id = req.params.id;
+    //   Author.findByPk(id).then(authors => {
+    //     if(authors != undefined){
+    //       Author.
+    //     }
+    //   })
+    // })
+
+    router.put("/authors/:id", (req, res) => {
+      var id = req.params.id;
+      var nm_autor = req.body.nm_autor; 
+      var nu_cpf = req.body.nu_cpf; 
+      var dt_nascimento = req.body.dt_nascimento;
+      var dt_cadastro = req.body.dt_cadastr;
+      var dt_desligamento = req.body.dt_desligamento;
+      var dt_ultima_atualizacao = req.body.dt_ultima_atualizacao;
+      
+      Author.update(
+        {
+        nm_autor:  nm_autor,
+        nu_cpf: nu_cpf,  
+        dt_nascimento: dt_nascimento, 
+        dt_cadastro: dt_cadastro, 
+        dt_desligamento: dt_desligamento, 
+        dt_ultima_atualizacao: dt_ultima_atualizacao
+      }, {
+        where: {
+          id_autor:id
+        }
+      }
+      ).then((data) => {
+          res.statusCode = 200;
+          res.json(data);
+      }).catch((err) => {
+        res.status(500).send({
+          message: "Erro ao enviar dados"
+        })
+      })
+    })
 
 module.exports = router;
